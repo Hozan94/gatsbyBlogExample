4dba14dbae6ab302dbdc7b2a6bfa3a912f923fc1
hozali <hozali@hozalis-MBP.lan>
2022-02-11 01:14:18 +0000

    adding style as SASS

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
-import * as styles from "./404.module.css"
+import * as styles from "./404.module.scss" //if importing styles from css module, out "import * as styles from ....."
 export default () => (
-  <div>
+  <div className={styles.content}>
     <h1 className={styles.header}>Page not found</h1>
     <p>The page you are looking for is not found</p>
   </div>
