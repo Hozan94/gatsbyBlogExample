b6cc518069ef520bebf79595d3407f9bebd1c0ef
hozali <hozali@hozalis-MBP.lan>
2022-02-20 23:22:44 +0000

    tidying up the files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>This is the index page</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci
+        velit, luctus vel pretium quis, eleifend vel purus. Praesent vel
+        dignissim turpis. Aliquam gravida ultrices pharetra. Morbi eu posuere
+        nisl.
+      </p>
+    </div>
+  )
 }
