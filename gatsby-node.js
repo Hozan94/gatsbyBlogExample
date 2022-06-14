280d8ece75d83b3e90d72d8a0055967e4ac04ccd
hozali <hozan949@gmail.com>
2022-06-14 11:11:57 +0100

    creating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === "MarkdownRemark") {
@@ -11,4 +12,41 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug,
     })
   }
+}
+
+//Create post pages programatically
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/post.js"),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
